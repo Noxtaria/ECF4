a00d9242083b834214b78e001199b57bf9c669b0
Kevin.B <kevin.buannec@proton.me>
2023-08-18 11:50:40 +0200

    Tentative SecurityConfig

--- src/main/java/com/example/ecf4_observatoire/application/security/SecurityConfig.java
+++ src/main/java/com/example/ecf4_observatoire/application/security/SecurityConfig.java
@@ -1,10 +1,51 @@
 package com.example.ecf4_observatoire.application.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private JWTTokenValidator jwtTokenValidator() {
+        return new JWTTokenValidator(jwtUtils);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/observations").authenticated()
+                .antMatchers(HttpMethod.PUT, "/observations/**").authenticated()
+                .anyRequest().permitAll()
+                .and()
+                .addFilterBefore(jwtTokenValidator(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
